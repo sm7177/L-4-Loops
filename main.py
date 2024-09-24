4d43429d18ee0d1890cd70a3cc2610db3828c376
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-24 17:02:11 +0545

    act

---
+++ main.py
@@ -0,0 +1,34 @@
+# str="codingal"
+
+# for char in str:
+#     print(char)
+
+
+# for i in range(1,11,2):
+#     print(i)
+    
+# num=int(input("Enter a number:"))
+
+# for i in range(1,11):
+#     # print(num,"x",i,"=",num*i)
+#     mul=num*i
+#     print("23x %d = %d "%(i,mul))
+    
+    # print(f"23x{i}={num*i}")
+    
+# n=int(input("Enter the number of rows:"))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+# num=sum(range(1,101))
+# print(num)
+
+num=1
+sum=0
+while(num<11):
+    sum=sum+num
+    num=num+1
+    
+print(sum)
